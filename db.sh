3a2140894191879293d9968fdcc6a89672eff9de
Dhatri <dhatri.venuturla@gmail.com>
2025-02-23 19:46:03 +0530

    Created frontend.sh file using shell

--- db.sh
+++ db.sh
@@ -15,5 +15,15 @@ validation $? "Enabling MySQL service"
 systemctl start mysqld &>>$log
 validation $? "Staring of MySQL service"
 
-mysql_secure_installation --set-root-pass $Password &>>$log
-validation $? "Setting of root user password"
+# mysql_secure_installation --set-root-pass $Password &>>$log
+# validation $? "Setting of root user password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.cloudevops.online -uroot -p${Password} -e 'show databases;' &>>$log
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${Password} &>>$log
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
